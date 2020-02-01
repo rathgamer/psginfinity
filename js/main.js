a8dd58bd4cddba38e155d2999a85c82d1bce3040
kundanranka <kunnuranka@gmail.com>
2020-02-01 22:54:51 +0530

    Sat Feb  1 22:54:51 IST 2020

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@
 	------------------------------------------------------ */
 	$(window).load(function () {
 		// will first fade out the loading animation 
-		$("#loader").fadeOut("slow", function () {
+		$("#loader1").fadeOut("slow", function () {
 
 			// will fade out the whole DIV that covers the website.
 			$("#preloader").delay(300).fadeOut("slow");
